f0de99f6fc30c1ac284cfc96594c220e8b551cd1
GH-Rumi <helmirizqiramadhan10@gmail.com>
2026-02-27 20:55:29 +0700

    Praktikum 1 - Route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM : 244107020107, Nama : Helmi Rizqi Ramadhan';
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId){
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id){
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name='John'){
+    return 'Nama saya '.$name;
 });
